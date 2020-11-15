78e3596d22a9e879908d29b2d01fbcf92dceebe4
kyungdong <rodejd23@gmail.com>
2020-11-16 00:42:23 +0900

    처음

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component{
 
     render() {
         return(
-            <div className={footer}>
+            <div>
                 2020 LOVELY KKYONG HOMEPAGE
             </div>
         )
